3da3c83407138f16cf0b735577400207988f2c5c
suriya <msgtosuriya@gmail.com>
2023-02-26 12:05:42 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Static("/", "./public")
+	app.Listen(":3000")
+}
